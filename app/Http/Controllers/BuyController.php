cbf67299bf5b49c8c219a77f8cf9d74820cc5c4c
Harvey Dobson <guesswho@harveydobson.co.uk>
2021-01-06 14:56:24 +0000

    Added Stripe Laravel Cashier integration - But can't test on local :-(

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -7,8 +7,11 @@ use Illuminate\Http\Request;
 class BuyController extends Controller
 {
     //
-    public function buy($purchase_reference)
+    public function buy(Request $request, $purchase_reference)
     {
+
+        //$stripeCustomer = $user->createAsStripeCustomer();
+
         $image_file = base_path() . '/digitalassets/HIGHRES/' . $purchase_reference . '.jpg';
 
         if(!file_exists($image_file))
@@ -16,7 +19,11 @@ class BuyController extends Controller
             return view('welcome')->with('status', 'Cannot find asset for that purchase reference, please try again.');
         }
 
-        return view('buy')->with('purchase_ref', $purchase_reference);
+
+        $request->user()->createAsStripeCustomer();
+
+        $url = $request->user()->billingPortalUrl();
+        return view('buy')->with('purchase_ref', $url);
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,13 @@ Route::get('/asset/{purchase_ref}', [App\Http\Controllers\AssetController::class
 
 Route::get('/asset/image/{purchase_ref}', [App\Http\Controllers\AssetController::class, 'image']);
 
-Route::get('/buy/{purchase_ref}', [App\Http\Controllers\BuyController::class, 'buy']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/buy/{purchase_ref}', [App\Http\Controllers\BuyController::class, 'buy']);
+
+    Route::get('/billing-portal', function (Request $request) {
+        return $request->user()->redirectToBillingPortal();
+    });
+});
 
 Route::get('login/{provider}', 'App\Http\Controllers\Auth\SocialController@redirectToProvider')->name('social.login');
 Route::get('login/{provider}/callback', 'App\Http\Controllers\Auth\SocialController@handleProviderCallback');
